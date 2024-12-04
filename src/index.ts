20c517ddfb6b792978a79c53cdbbeb749b0860a3
samueldevrel <samuelmwangi045@gmail.com>
2024-12-04 21:35:30 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import { IDL, query, update } from 'azle';
 
-export default class {
-    message: string = 'Hello world!';
-
-    @query([], IDL.Text)
-    getMessage(): string {
-        return this.message;
-    }
-
-    @update([IDL.Text])
-    setMessage(message: string): void {
-        this.message = message;
-    }
-}
 import {
   Canister,
   Err,
